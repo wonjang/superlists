ff7e7d480d4a389bc1dcc70702bac488c7e22bc9
wonjang <won.jang81@gmail.com>
2017-05-28 16:09:38 +0900

    기능테스트가 작업아이템을 추가할수 있는지 확인

--- functional_test.py
+++ functional_test.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 from selenium import webdriver
 import unittest
 
@@ -10,7 +11,25 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do',self.browser.title)
-        self.fail()
+        header_text = self.browser.find_elements_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            '작업아이텀입력'
+        )
+
+        inputbox.send_keys('공작깃털사기')
+        inputbox.send_keys(Keys.Enter)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: 공작깃털 사기' for row in rows),
+        )
+
+        self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main()
