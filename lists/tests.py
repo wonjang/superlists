f9bf6172e5b00eba3d111e6938560a2a35fc4a86
wonjang <won.jang81@gmail.com>
2017-05-28 16:31:21 +0900

    템플릿을 이용하도록 홈페이지 뷰를 리펙터링

--- lists/tests.py
+++ lists/tests.py
@@ -1,18 +1,20 @@
 #-*- coding: utf-8 -*-
 
 from django.core.urlresolvers import resolve
-from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
+from django.test import TestCase
+
 from lists.views import home_page
+
 
 class HomePageTest(TestCase):
     def test_root_url_resolve_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func,home_page)
 
-    def test_home_page_return_correct_html(self):
+    def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>',response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(),expected_html)
